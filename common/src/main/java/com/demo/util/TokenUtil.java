55b8f46f09c038d8aaa403f003e1dd636ed1d9b8
licheng <2501093733@qq.com>
2020-12-19 09:26:56 +0800

    token的调用和生成

--- common/src/main/java/com/demo/util/TokenUtil.java
+++ common/src/main/java/com/demo/util/TokenUtil.java
@@ -16,18 +16,29 @@ import java.io.Serializable;
  * @date 2020/12/18 16:48
  */
 public class TokenUtil  {
+    /** 密钥串 **/
     public static String secretKey = "izVguZPRsBQ5Rqw6dhMvcIwy8_9lQnrO3vpxGwP";
 
+    /**
+     * @author lc
+     * @date 2020/12/19
+     * @description 生成token
+     */
     public static String getToken(Serializable userId){
         try {
             Algorithm algorithm = Algorithm.HMAC256(secretKey);
-            return JWT.create()
+            return JWT.create().withKeyId(userId.toString())
                     .withIssuer("auth0")
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new ServiceException("token生成失败");
         }
     }
+    /**
+     * @author lc
+     * @date 2020/12/19
+     * @description 获取userId
+     */
     public static String getUserId(String token){
         try {
             Algorithm algorithm = Algorithm.HMAC256(secretKey);
@@ -35,13 +46,14 @@ public class TokenUtil  {
                     .withIssuer("auth0")
                     .build(); //Reusable verifier instance
             DecodedJWT jwt = verifier.verify(token);
-            return jwt.getSubject();
+            return jwt.getKeyId();
         } catch (JWTVerificationException exception){
             throw new ServiceException("token解密失败");
         }
     }
 
     public static void main(String[] args) {
+        //测试
         String token = getToken(1);
         System.out.println(token);
         String userId = getUserId(token);
