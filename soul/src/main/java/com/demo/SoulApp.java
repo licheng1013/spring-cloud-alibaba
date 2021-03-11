e281a1e99e113ea0340c4b6d647fcb812a789f8c
licheng <2501093733@qq.com>
2021-03-11 15:18:47 +0800

    111优化

---
+++ soul/src/main/java/com/demo/SoulApp.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author root
+ * @description TODO
+ * @date 2021/3/11 14:58
+ */
+
+@SpringBootApplication
+public class SoulApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SoulApp.class, args);
+    }
+}
