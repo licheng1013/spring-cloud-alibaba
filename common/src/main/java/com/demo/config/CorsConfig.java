42fb2c2bb1e234cb52116df9515634d872bc2382
licheng <2501093733@qq.com>
2021-03-13 16:53:50 +0800

    跨域配置, 通过gateway框架来转发,gateway如果配置了跨域,则其他转发项目中不需要配置跨域,

--- common/src/main/java/com/demo/config/CorsConfig.java
+++ common/src/main/java/com/demo/config/CorsConfig.java
@@ -1,39 +1,33 @@
 package com.demo.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;  
-import org.springframework.web.cors.CorsConfiguration;  
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;  
-import org.springframework.web.filter.CorsFilter;
-
-import java.time.Duration;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author lc
  * @date 2021/1/11
- * @description 跨域配置
+ * @description 跨域配置, 通过gateway框架来转发,gateway如果配置了跨域,则其他转发项目中不需要配置跨域,
  */
-@Configuration  
+@Configuration
 public class CorsConfig {  
-    private CorsConfiguration buildConfig() {  
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        // 1允许任何域名使用
-        corsConfiguration.addAllowedOrigin("*");
-        // 2允许任何头
-        corsConfiguration.addAllowedHeader("*");
-        // 3允许ajax异步请求带cookie信息
-        corsConfiguration.setAllowCredentials(true);
-        // 4允许任何方法（post、get等）
-        corsConfiguration.addAllowedMethod("*");
-        // 5设置OPTIONS预检请求的过期时间
-        corsConfiguration.setMaxAge(Duration.ofDays(1));
-        return corsConfiguration;  
-    }  
-  
-    @Bean  
-    public CorsFilter corsFilter() {  
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();  
-        source.registerCorsConfiguration("/**", buildConfig());  
-        return new CorsFilter(source);  
-    }  
+//    private CorsConfiguration buildConfig() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        // 1允许任何域名使用
+//        corsConfiguration.addAllowedOrigin("*");
+//        // 2允许任何头
+//        corsConfiguration.addAllowedHeader("*");
+//        // 3允许ajax异步请求带cookie信息
+//        corsConfiguration.setAllowCredentials(true);
+//        // 4允许任何方法（post、get等）
+//        corsConfiguration.addAllowedMethod("*");
+//        // 5设置OPTIONS预检请求的过期时间
+//        corsConfiguration.setMaxAge(Duration.ofDays(1));
+//        return corsConfiguration;
+//    }
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", buildConfig());
+//        return new CorsFilter(source);
+//    }
 }
