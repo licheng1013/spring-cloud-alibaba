eb3a7b2edccbe10c4366f977272db7df00bca347
licheng <2501093733@qq.com>
2021-03-15 15:12:37 +0800

    111

--- gateway/src/main/java/com/demo/GatewayPropertiesConfig.java
+++ gateway/src/main/java/com/demo/GatewayPropertiesConfig.java
@@ -3,17 +3,16 @@ package com.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.config.GatewayProperties;
-import org.springframework.cloud.gateway.handler.predicate.PredicateDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author root
@@ -25,42 +24,42 @@ import java.util.List;
 @RestController
 @RefreshScope
 public class GatewayPropertiesConfig extends GatewayProperties {
+    private static final List<RouteDefinition> list = new ArrayList<>();
 
     @Value("${user.msg}")
     private String msg;
 
-    private static final List<RouteDefinition> list = new ArrayList<>();
+    @Value("${user.list}")
+    public void setTest(String[] list) {
+        List<Map<String, Object>> maps = Parsing(list);
+        System.out.println(maps);
+
+    }
+
+    public List<Map<String, Object>> Parsing(String[] list) {
+        List<Map<String, Object>> maps = new ArrayList<>();
+        for (String s : list) {
+            String[] split = s.split(";"); //分隔符
+            Map<String, Object> map = new HashMap<>();
+            for (String v : split) {
+                String[] t = v.split(":"); //分隔符
+                Object l =  t[1];
+                if ("path".equals(t[0])){
+                    String[] j = t[1].split("-");
+                    if (j.length != 0) { //至少有一个
+                        l = j;
+                    }
+                }
+                map.put(t[0], l);
+            }
+            maps.add(map);
+        }
+        return maps;
+    }
+
 
     @Override
     public List<RouteDefinition> getRoutes() {
-        return list;
-    }
-
-
-
-    @GetMapping("add")
-    public Object add() {
-        RouteDefinition definition = new RouteDefinition();
-        definition.setId("user-route");
-        try {
-            definition.setUri(new URI("lb://alibaba-user"));
-        } catch (URISyntaxException e) {
-        }
-        ArrayList<PredicateDefinition> p = new ArrayList<>();
-        p.add(new PredicateDefinition("Path=/user/**"));
-        definition.setPredicates(p);
-        list.add(definition);
-        return list;
-    }
-
-    @GetMapping("get")
-    public Object get() {
-        return list;
-    }
-
-    @GetMapping("del")
-    public Object delete() {
-        list.clear();
         return list;
     }
 
