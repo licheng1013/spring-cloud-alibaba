2cfc8fdadebafa032d19d2a2cb51a77eed2a46e9
licheng <2501093733@qq.com>
2021-03-15 15:30:02 +0800

    优化

--- gateway/src/main/java/com/demo/GatewayPropertiesConfig.java
+++ gateway/src/main/java/com/demo/GatewayPropertiesConfig.java
@@ -3,12 +3,15 @@ package com.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.config.GatewayProperties;
+import org.springframework.cloud.gateway.handler.predicate.PredicateDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -24,16 +27,43 @@ import java.util.Map;
 @RestController
 @RefreshScope
 public class GatewayPropertiesConfig extends GatewayProperties {
-    private static final List<RouteDefinition> list = new ArrayList<>();
+    private static  List<RouteDefinition> list = new ArrayList<>();
 
     @Value("${user.msg}")
     private String msg;
+    public static final String ID = "id"; // id
+    public static final String URI = "uri"; // uri
+    public static final String predicates = "path"; // 路径
 
     @Value("${user.list}")
     public void setTest(String[] list) {
-        List<Map<String, Object>> maps = Parsing(list);
-        System.out.println(maps);
+        List<RouteDefinition> routeDefinitions = new ArrayList<>();
 
+        List<Map<String, Object>> maps = Parsing(list);
+        maps.forEach(i -> {
+            RouteDefinition definition = new RouteDefinition();
+            definition.setId(i.get(ID).toString());
+            try {
+                definition.setUri(new URI(i.get(URI).toString()));
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+            ArrayList<PredicateDefinition> p = new ArrayList<>();
+            if (i.get(predicates) instanceof String[]){
+                String[] o = (String[]) i.get(predicates);
+                for (String s : o) {
+                    p.add(new PredicateDefinition(s));
+                }
+            }else {
+                p.add(new PredicateDefinition(i.get(predicates).toString()));
+            }
+
+            definition.setPredicates(p);
+            routeDefinitions.add(definition);
+        });
+
+        GatewayPropertiesConfig.list = routeDefinitions;
+        System.out.println(GatewayPropertiesConfig.list);
     }
 
     public List<Map<String, Object>> Parsing(String[] list) {
@@ -43,8 +73,8 @@ public class GatewayPropertiesConfig extends GatewayProperties {
             Map<String, Object> map = new HashMap<>();
             for (String v : split) {
                 String[] t = v.split(":"); //分隔符
-                Object l =  t[1];
-                if ("path".equals(t[0])){
+                Object l = t[1];
+                if ("path".equals(t[0])) {
                     String[] j = t[1].split("-");
                     if (j.length != 0) { //至少有一个
                         l = j;
