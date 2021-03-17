565b2045f94ce8abb1b6ac74060f1aebd303afc3
licheng <2501093733@qq.com>
2021-03-17 16:41:14 +0800

    优化随机分钟

--- core/redis/src/main/java/com/demo/annotation/Cache.java
+++ core/redis/src/main/java/com/demo/annotation/Cache.java
@@ -14,10 +14,10 @@ public @interface Cache {
     /**
      * @return 默认起始随机时间,单位(秒)
      */
-    long minTime() default 30;
+    long minTime() default 1800;
 
     /**
      * @return 默认结束随机时间,单位(秒),时间必须大于起始时间,否则以起始时间为随机数即1-30秒
      */
-    long maxTime() default 60;
+    long maxTime() default 3600;
 }
