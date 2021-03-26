eb116c1b9b9e4112346da71c747688525671aeec
licheng <2501093733@qq.com>
2021-03-26 14:28:16 +0800

    学习rocketMq示例

---
+++ rocketmq/rocketmq-push/src/main/java/com/demo/RocketMqApp.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author root
+ * @description TODO
+ * @date 2021/3/26 14:24
+ */
+@SpringBootApplication
+public class RocketMqApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RocketMqApp.class, args);
+    }
+}
