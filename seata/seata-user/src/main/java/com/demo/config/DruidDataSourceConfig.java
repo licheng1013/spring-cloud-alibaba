5db4fca2c7e036e189c1af834a5f3b5d1fbdb50d
licheng <2501093733@qq.com>
2021-04-13 16:15:48 +0800

    修改数据库配置

--- seata/seata-user/src/main/java/com/demo/config/DruidDataSourceConfig.java
+++ seata/seata-user/src/main/java/com/demo/config/DruidDataSourceConfig.java
@@ -1,39 +1,36 @@
 package com.demo.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-import javax.sql.DataSource;
-
 /**
  * @author root
  * @description TODO
- * @date 2021/4/13 13:50
+ * @date 2021/4/13 15:40
  */
-
 @Configuration
 @Primary
 @RefreshScope
 @ConfigurationProperties(prefix = "config.db")
 @Slf4j
-public class DataSourcePropertiesConfig extends DataSourceProperties {
-
-    @Override
-    public void setType(Class<? extends DataSource> type) {
-        super.setType(type);
-    }
-
+@Setter
+@Getter
+public class DruidDataSourceConfig extends DruidDataSource{
     @Override
     public void setUrl(String url) {
+        log.info("链接地址: {}",url);
         super.setUrl(url);
     }
 
     @Override
     public void setUsername(String username) {
+        log.info("用户名: {}",username);
         super.setUsername(username);
     }
 
