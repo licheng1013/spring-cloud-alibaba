62de849780b928d87eb9cf29232bba6454a940b1
licheng <2501093733@qq.com>
2021-04-13 14:36:44 +0800

    优化

--- seata/seata-user/src/main/java/com/demo/config/DataSourcePropertiesConfig.java
+++ seata/seata-user/src/main/java/com/demo/config/DataSourcePropertiesConfig.java
@@ -1,10 +1,13 @@
 package com.demo.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 /**
  * @author root
@@ -15,8 +18,28 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 @Primary
 @RefreshScope
-@ConfigurationProperties(prefix = "config")
+@ConfigurationProperties(prefix = "config.db")
 @Slf4j
-public class DataSourcePropertiesConfig {
+public class DataSourcePropertiesConfig extends DataSourceProperties {
 
+    @Override
+    public void setType(Class<? extends DataSource> type) {
+        super.setType(type);
+    }
+
+    @Override
+    public void setUrl(String url) {
+        super.setUrl(url);
+    }
+
+    @Override
+    public void setUsername(String username) {
+        super.setUsername(username);
+    }
+
+    @Override
+    public void setPassword(String password) {
+        log.info("密码: {}",password);
+        super.setPassword(password);
+    }
 }
