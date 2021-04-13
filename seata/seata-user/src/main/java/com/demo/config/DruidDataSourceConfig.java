2a1860aa7016097485a5463a653867ea6efb7544
licheng <2501093733@qq.com>
2021-04-13 16:39:51 +0800

    11

--- seata/seata-user/src/main/java/com/demo/config/DruidDataSourceConfig.java
+++ seata/seata-user/src/main/java/com/demo/config/DruidDataSourceConfig.java
@@ -6,8 +6,11 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 /**
  * @author root
@@ -21,22 +24,46 @@ import org.springframework.context.annotation.Primary;
 @Slf4j
 @Setter
 @Getter
-public class DruidDataSourceConfig extends DruidDataSource {
-    @Override
-    public void setUrl(String url) {
-        log.info("链接地址: {}",url);
-        super.setUrl(url);
-    }
+public class DruidDataSourceConfig  {
+    private static String url ;//= "jdbc:mysql://192.168.1.5:3306/t_user?useUnicode=true&characterEncoding=utf-8&serverTimezone=Asia/Shanghai";
+    private static String username ;//= "root";
+    private static String password ;//= "root";
 
-    @Override
-    public void setUsername(String username) {
-        log.info("用户名: {}",username);
-        super.setUsername(username);
-    }
-
-    @Override
+//    @Override
+//    public void setUrl(String url) {
+//        log.info("链接地址: {}",url);
+//        super.setUrl(url);
+//    }
+//
+//    @Override
+//    public void setUsername(String username) {
+//        log.info("用户名: {}",username);
+//        super.setUsername(username);
+//    }
+//
+//    @Override
     public void setPassword(String password) {
-        log.info("密码: {}",password);
-        super.setPassword(password);
+        log.info("password: {}",password);
+        DruidDataSourceConfig.password = password;
+    }
+    public void setUsername(String username) {
+        log.info("username: {}",username);
+        DruidDataSourceConfig.username = username;
+    }
+    public void setUrl(String url) {
+        log.info("url: {}",url);
+        DruidDataSourceConfig.url = url;
+    }
+
+
+    @Bean
+    @RefreshScope
+    public DataSource db(){
+        log.info("初始化数据库:....");
+        DruidDataSource db = new DruidDataSource();
+        db.setUsername(username);
+        db.setPassword(password);
+        db.setUrl(url);
+        return db;
     }
 }
