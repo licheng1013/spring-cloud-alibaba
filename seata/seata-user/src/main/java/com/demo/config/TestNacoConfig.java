55d6f8386f611cd736c2a9fbf53921cba692a714
licheng <2501093733@qq.com>
2021-04-14 11:29:26 +0800

    111

--- seata/seata-user/src/main/java/com/demo/config/TestNacoConfig.java
+++ seata/seata-user/src/main/java/com/demo/config/TestNacoConfig.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author root
@@ -17,10 +20,16 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "config.test")
 @Getter
 @Slf4j
+@RestController
+@RequestMapping("user")
 public class TestNacoConfig {
     private  String name;
     public void setName(String name){
         log.info("刷新: {}",name);
         this.name= name;
+    }
+    @GetMapping("get/name")
+    public Object get(){
+        return this.name;
     }
 }
