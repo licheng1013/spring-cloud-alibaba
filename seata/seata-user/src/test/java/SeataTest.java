551ff24801cce99aee58eb24bd8ee6e0419663e2
licheng <2501093733@qq.com>
2021-04-16 14:49:50 +0800

    1111

--- seata/seata-user/src/test/java/SeataTest.java
+++ seata/seata-user/src/test/java/SeataTest.java
@@ -1,36 +1,41 @@
-import cn.hutool.core.convert.Convert;
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.RandomUtil;
+import lombok.extern.slf4j.Slf4j;
 
-import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
 
 /**
  * @author root
  * @description TODO
  * @date 2021/4/13 14:49
  */
+@Slf4j
 public class SeataTest {
-    public static void main(String[] args) {
-//        DateTime t1 = DateUtil.parse("2021-1-10", DatePattern.NORM_DATE_PATTERN);
-//        Date t2 = new Date();
-//        //计算两天相差数....
-//        long day = DateUtil.betweenDay(t1, t2, false);
-//        int [] s = {60,90,120,150,180};
-//        HashMap<String, Boolean> map = new HashMap<>();
-//        map.put("1", false);
-//        map.put("2", false);
-//        map.put("3", false);
-//        map.put("4", false);
-//        map.put("5", false);
-//        for (int i = 0;i < s.length;i++) {
-//            if (day > s[i] ) {
-//                if (map.get((i+1+""))) {
-//                    System.out.println("已领取");
-//                }else {
-//                    System.out.println("未领取");
-//                }
-//            }else {
-//                System.out.println("领取条件未到");
-//            }
-//        }
-        System.out.println(Convert.hexToStr("e68891e698afe4b880e4b8aae5b08fe5b08fe79a84e58fafe788b1e79a84e5ad97e7aca6e4b8b2" , StandardCharsets.UTF_8));
+    public static void main(String[] args) throws InterruptedException {
+//        System.out.println(Convert.hexToStr("e68891e698afe4b880e4b8aae5b08fe5b08fe79a84e58fafe788b1e79a84e5ad97e7aca6e4b8b2" , StandardCharsets.UTF_8));
+        int n = 60;
+        long sleepTime = 6000;
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
+            String t = DateUtil.format(new Date(), DatePattern.NORM_DATETIME_MINUTE_PATTERN);
+            int r = RandomUtil.randomInt(1, 1001);
+            if  (r <= 6) {
+                if (map.get(t) == null) {
+                    String v = DateUtil.format(new Date(), DatePattern.NORM_DATETIME_MINUTE_PATTERN);
+                    if (t.equals(v)) {
+                        map.put(t, 1);
+                    }
+                }else {
+                    String v = DateUtil.format(new Date(), DatePattern.NORM_DATETIME_MINUTE_PATTERN);
+                    Integer integer = map.get(t);
+                    if (t.equals(v)) {
+                        map.put(t, integer+1);
+                    }
+                }
+            }
+        }
+        log.info("随机结果: {}", map);
     }
 }
