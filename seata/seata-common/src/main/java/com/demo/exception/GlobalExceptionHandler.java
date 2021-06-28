f8a7a32e100ca722bee6c99a6e8ff03bd5ad07bc
licheng <2501093733@qq.com>
2021-06-28 16:24:53 +0800

    优化异常处理

---
+++ seata/seata-common/src/main/java/com/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.demo.exception;
+
+import com.demo.util.JsonResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 异常处理
+ * @author lc
+ * @date 2020/12/18
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+  /**
+   * @author lc
+   * @date 2020/12/18
+   * @description 自定义业务异常处理
+   */
+    @ExceptionHandler(ServiceException.class)
+    public JsonResult<String> doHandleServiceException(ServiceException e){
+        return JsonResult.fail(e.getMessage());
+    }
+
+    /**
+     * @author lc
+     * @date 2020/12/18
+     * @description 系统异常处理
+     */
+    @ExceptionHandler(Exception.class)
+    public JsonResult<String> doHandleServiceException(Exception e){
+        e.printStackTrace();
+        return JsonResult.fail("系统繁忙");
+    }
+}
